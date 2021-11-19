75912bd658c1ceea551faf709467bc2fded7efe7
Mackenzie Weaver <macknz7@gmail.com>
2021-11-19 18:55:44 -0500

    Initial commit

---
+++ Models/BTUser.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Identity;
+
+namespace TheBugTracker.Models
+{
+    public class BTUser : IdentityUser
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Full Name")]
+        public string FullName { get => $"{FirstName} {LastName}"; }
+    }
+}
