6c797eb0eb24171f6008338b739c75731c62e16b
Mackenzie Weaver <macknz7@gmail.com>
2022-01-22 16:37:58 -0500

    invite stuff

--- Controllers/InvitesController.cs
+++ Controllers/InvitesController.cs
@@ -1,21 +1,26 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using TheBugTracker.Data;
 using TheBugTracker.Models;
+using TheBugTracker.Services;
 
 namespace TheBugTracker.Controllers
 {
     public class InvitesController : Controller
     {
         private readonly ApplicationDbContext _context;
+        private readonly IEmailSender _emailService;
 
-        public InvitesController(ApplicationDbContext context)
+        public InvitesController(ApplicationDbContext context, IEmailSender emailService)
         {
+            _emailService = emailService;
             _context = context;
         }
 
@@ -49,33 +54,42 @@ namespace TheBugTracker.Controllers
         }
 
         // GET: Invites/Create
-        public IActionResult Create()
+        public async Task<IActionResult> Create()
         {
-            ViewData["CompanyId"] = new SelectList(_context.Companies, "Id", "Name");
-            ViewData["InviteeId"] = new SelectList(_context.Users, "Id", "Id");
-            ViewData["InvitorId"] = new SelectList(_context.Users, "Id", "Id");
-            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Name");
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == User.Identity.Name);
+            if (user.CompanyId == null) return RedirectToAction("Index", "Users");
+            ViewData["CompanyId"] = user.CompanyId;
+            ViewData["ProjectId"] = new SelectList(_context.Projects
+                .Where(x => x.CompanyId == user.CompanyId), "Id", "Name");
+            return View();
+        }
+        
+        public async Task<IActionResult> Accept()
+        {
+            // if user is signed in then sign them out
+
+            // have newly accepted user register
+             
+            // once registered, automatically joins company   
+        
             return View();
         }
 
-        // POST: Invites/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,InviteDate,JoinDate,CompanyToken,CompanyId,ProjectId,InvitorId,InviteeId,InviteeEmail,InviteeFirstName,InviteeLastName,IsValid")] Invite invite)
+        public async Task<IActionResult> Create(Invite invite)
         {
-            if (ModelState.IsValid)
-            {
-                _context.Add(invite);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["CompanyId"] = new SelectList(_context.Companies, "Id", "Name", invite.CompanyId);
-            ViewData["InviteeId"] = new SelectList(_context.Users, "Id", "Id", invite.InviteeId);
-            ViewData["InvitorId"] = new SelectList(_context.Users, "Id", "Id", invite.InvitorId);
-            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Name", invite.ProjectId);
-            return View(invite);
+            var company = await _context.Companies.FirstOrDefaultAsync(x => x.Id == invite.CompanyId);
+            var callbackUrl = $"{Request.Scheme}://{Request.Host}/Invites/Accept";
+            await _emailService.SendEmailAsync(
+                invite.InviteeEmail, 
+                $"Invitation to Join {company.Name}",
+                $"<a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>Accept</a>."
+            );
+            
+            // _context.Add(invite);
+            // await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Users");
         }
 
         // GET: Invites/Edit/5
