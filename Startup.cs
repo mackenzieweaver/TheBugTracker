92e0f6d57e898adfeef630de14d23345238b1d96
Mackenzie Weaver <macknz7@gmail.com>
2022-06-05 14:07:06 -0400

    global config to use splitquery

--- Startup.cs
+++ Startup.cs
@@ -52,8 +52,10 @@ namespace TheBugTracker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseNpgsql(GetConnectionString(Configuration)));
+            services.AddDbContext<ApplicationDbContext>(dbOptions =>
+                dbOptions.UseNpgsql(GetConnectionString(Configuration),
+                sqlOptions => sqlOptions.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)
+            ));
             
             services.AddDatabaseDeveloperPageExceptionFilter();
 
